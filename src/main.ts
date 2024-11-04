49b568bdd47f4b07876cf44715f6706c94f0476e
hrong1 <hrong1@ucsc.edu>
2024-11-04 08:09:22 -0800

    add button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 // todo
+import "./style.css";
+ 
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = "Demo 3";
+const header = document.createElement("h1");
+header.innerHTML = "Demo 3";
+app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Click Me";
+app.append(button);
+
+button.addEventListener("click", () => {
+    alert('You just clicked the button!')
+});
